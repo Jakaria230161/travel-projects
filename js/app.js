c1700426f29430786f7d873e467f1b46cac21776
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-20 14:11:32 +0600

    fair section committed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // vehicles section with Object start from here 
+// create an object
 let carObject = {
     vehicle: "Car",
     imageUrl:
@@ -29,34 +30,89 @@ let busObject = {
         "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 };
 
-
+/*
+1,access the main section by using id
+2,create a div element by createElement
+3,create innerHTML of the above div dynamically with service
+4,after that appendChild to the main section
+*/
 
 function displayServices(service) {
-
+    const stringified = JSON.stringify(service);
     const mainSection = document.getElementById("main-section");
     const div = document.createElement("div");
     div.innerHTML = `
     <!-- card segment here  -->
-            <div class="card mt-3 mx-auto" style="max-width: 800px;">
+            <div class="card mt-3 mx-auto" style="max-width: auto;">
                 <div class="row g-0">
                     <div class="col-md-4">
                         <img src=${service.imageUrl} class="img-fluid rounded-start h-100" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                            <h5 class="card-title">Transport Mood : ${service.vehicle}</h5>
+                            <h5 class="card-title">Transport Mode : ${service.vehicle}</h5>
                             <p class="card-text">${service.description}</p>
                             <p class="card-text"><small class="text-muted">Fare per kilo cost : ${service.farePerKilo}</small></p>
                             <p class="card-text"><small class="text-muted">capacity of vehicles : ${service.capacity}</small></p>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" onclick ='handleBooking(${stringified});' data-bs-target="#exampleModal">
+                                See More...
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>`
     
     mainSection.appendChild(div);
-    console.log(service);
+    // console.log(service);
 }
 
 displayServices(carObject);
 displayServices(busObject);
 displayServices(bikeObject);
+
+
+// handle booking or buy
+/*
+1, write the function name handleBooking
+2,it will be have an object as parameter
+*/
+
+function handleBooking(object) {
+    const stringified = JSON.stringify(object);
+    const modalContent = document.getElementById("modal-content");
+
+    let vehicle = "Bike"
+    let id = vehicle + "-fare-input"
+
+    modalContent.innerHTML = `
+    <div class="card mx-auto" style="width: auto;">
+    <img src=${object.imageUrl} class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Vehicles Mode : ${object.vehicle}</h5>
+    <p class="card-text">${object.description}</p>
+    <p class="card-text">${object.description}</p>
+    <p class="card-text"><small class="text-muted">Fare per kilo cost : ${object.farePerKilo}</small></p>
+    <p class="card-text"><small class="text-muted">capacity of vehicles : ${object.capacity}</small></p>
+
+    <p>Fare : <small class="text-muted" id="fare"> </small></p>
+    <p>Tax : <small class="text-muted" id="tax"> </small></p>
+    <p>Total-Cost : <small class="text-muted" id="total-cost"> </small></p>
+        <div class="d-flex flex-column" role="search">
+            <input class="form-control m-2" type="number" id="distance-input" placeholder="How many kilometers will you go ?" aria-label="Search">
+            <input class="form-control m-2" type="number" id="quantity-input" placeholder="How many vehicles do you need ?" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit" onclick='calculateCost(${stringified});'>Submit</button>
+        </div>
+    </div>
+</div>
+    `
+}
+
+function calculateCost(object) {
+    const quantity = document.getElementById("quantity-input").value;
+    const distance = document.getElementById("distance-input").value;
+    console.log(quantity, distance);
+    const fareVehicle = document.getElementById("fare");
+    fareVehicle.innerHTML = quantity * distance * object.farePerKilo;
+    console.log(fareVehicle);
+}
